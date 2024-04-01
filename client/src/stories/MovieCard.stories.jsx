259bf4379305a02a5ced7242f55152c845d0408f
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-04-01 18:44:01 +0200

    add selected movies

--- client/src/stories/MovieCard.stories.jsx
+++ client/src/stories/MovieCard.stories.jsx
@@ -5,7 +5,7 @@ import { movies } from './stub.js';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Movie card',
+  title: 'Card/Movie card',
   component: MovieCard
 };
 
