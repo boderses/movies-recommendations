2ca3513a88e9f2ac6b92b2619c1220b0d4f7f581
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-04-12 16:25:28 +0200

    add testing of custome hook "useMovies"

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const client = new ApolloClient({
     uri: "http://localhost:4000/",
     cache: new InMemoryCache(),
+    connectToDevTools: true
   });
   return (
     <ApolloProvider client={client}>
