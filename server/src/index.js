df2fcafd413e5ba54b5fb52688dc51bec432df88
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-08-12 20:00:53 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -56,4 +56,4 @@ async function startApolloServer(typeDefs, resolvers) {
 //   console.log(`🚀 Server ready at http://localhost:${4000}${server.graphqlPath}`);
 // }
 
-startApolloServer(typeDefs, resolvers)
+startApolloServer(typeDefs, resolvers);
